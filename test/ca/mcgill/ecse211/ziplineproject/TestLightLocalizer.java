3e91e6634604e120599be048ccbe0d458f99bf23
estherliu <chaoyi97@gmail.com>
2017-11-09 19:57:58 -0500

    11.9

--- test/ca/mcgill/ecse211/ziplineproject/TestLightLocalizer.java
+++ test/ca/mcgill/ecse211/ziplineproject/TestLightLocalizer.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse211.ziplineproject;
 
+import lejos.hardware.Button;
 import lejos.hardware.lcd.TextLCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.sensor.EV3ColorSensor;
@@ -17,18 +18,35 @@ public class TestLightLocalizer {
     private static Odometer odometer = Main.odometer;
     private static Navigation navigation = Main.navigation;
     private static Display display = Main.display;
+    private static TextLCD textLCD = Main.textLCD;
 
     public static void testLightLocalizer() {
+    	 // Wait for button press
+        int button = 0;
+        textLCD.drawString("Press a button ", 0, 0);
+        textLCD.drawString("to start the ", 0, 1);
+        textLCD.drawString("navigation test", 0, 2);
+        
+        while (button == 0)
+            button = Button.waitForAnyPress();
+        
+        // Clear display
+        textLCD.clear();
+        
+        // Start odometer and display
         odometer.start();
         display.start();
+       
         
         // Set odometer to start at (1,1)
         odometer.setX(1*TILE); odometer.setY(1*TILE);
         
-        navigation.travelTo(2, 3);
-        navigation.travelTo(3, 2);
+        //navigation.travelTo(2, 3);
+        //navigation.travelTo(3, 2);
         
-        (new LightLocalizer()).doLightLocalization(0, 0);
+        LightLocalizer lo=new LightLocalizer();
+        //lo.dotest();
+        lo.doLightLocalizationNew(3, 2);
         
         // TODO Assert result
     }
